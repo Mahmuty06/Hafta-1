66b8c2feded8bc567102d6e5919f0fbd9d88c0ae
Mahmuty06 <142676359+Mahmuty06@users.noreply.github.com>
2023-09-17 02:54:14 +0300

    Add files via upload

---
+++ AsalSayiBulma/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int sayac = 0 ,kontrol = 0;
+
+        for(int sayi=2;sayi<=100;sayi++){
+            for (int i = 2; i < sayi; i++){
+
+                 if (sayi % i == 0){
+                    kontrol = 1;
+                    break;
+                }
+            }
+            if
+            (kontrol==0) {
+                System.out.print(sayi+"\n");
+                sayac++;
+            }
+        }
+
+    }
+}
