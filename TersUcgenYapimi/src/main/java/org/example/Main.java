0469249083e33fbb0c9374a57f5b37801af836c4
Mahmuty06 <142676359+Mahmuty06@users.noreply.github.com>
2023-09-17 21:05:58 +0300

    Add files via upload

---
+++ TersUcgenYapimi/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        int sayac = 10;
+        for(int j = 0 ; j < 10 ; j ++){
+        for (int i =sayac ; i > 0; i--) {
+
+            System.out.print("*");
+
+        }
+            System.out.println("");
+        sayac --;
+    }
+}}
