9d87c1786af5492817f0a58ebe0cd4dbf15b4a95
Mahmuty06 <142676359+Mahmuty06@users.noreply.github.com>
2023-09-17 18:21:11 +0300

    Add files via upload

---
+++ MukemmelSayiBulma/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayi, toplam = 0;
+
+        System.out.println("Lütfen Bir Sayı Giriniz : ");
+        sayi = input.nextInt();
+
+        for (int i = 1; i < sayi; i++) {
+            if (sayi % i == 0) {
+                toplam = toplam + i;
+            }
+        }
+        if (sayi == toplam) {
+            System.out.println(sayi + " Bu Sayı En Mükemmeldir .");
+        } else {
+            System.out.println(sayi + " Bu Sayı Mükemmel Sayı Değildir.");
+        }
+    }
+}
