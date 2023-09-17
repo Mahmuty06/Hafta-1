2ed2b2e2671e95b1fd829740a687f0e910301e54
Mahmuty06 <142676359+Mahmuty06@users.noreply.github.com>
2023-09-17 21:43:56 +0300

    Update Main.java

--- AsalSayiBulma/src/main/java/org/example/Main.java
+++ AsalSayiBulma/src/main/java/org/example/Main.java
@@ -2,11 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int sayac = 0 ,kontrol = 0;
+        int sayac = 0 ;
 
         for(int sayi=2;sayi<=100;sayi++){
+            int kontrol = 0;
             for (int i = 2; i < sayi; i++){
-
                  if (sayi % i == 0){
                     kontrol = 1;
                     break;
